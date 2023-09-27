bb432d63829d14fed4725c29d7d24876319c3cfe
Tejas Patel <pateltejas.2005@gmail.com>
2023-09-28 00:26:45 +0530

    first commit

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -63,9 +63,9 @@ const Comments = ({ postSlug }) => {
             <div className='mt-7 flex flex-col'>
                 {isLoading
                     ? "loading"
-                    : data?.map((comment) => {
+                    : data?.map((comment, idx) => {
                         return (
-                            <div className='flex flex-col gap-10 border border-b-[1px] border-t-0 border-x-0 py-5 px-5'>
+                            <div key={idx} className='flex flex-col gap-10 border border-b-[1px] border-t-0 border-x-0 py-5 px-5'>
                                 <div className='flex flex-col gap-3 text-sm'>
                                     <div className='flex gap-3 items-center'>
                                         <Image src={comment.user.image} height={50} width={50} alt="author_image" className="aspect-square object-cover rounded-full h-10 w-10" />
