8437104ab9d5f1507fe4e08fbf296392a3eaf3dc
Tejas Patel <pateltejas.2005@gmail.com>
2023-09-28 14:42:33 +0530

    first commit

---
+++ src/app/write/page copy 2.jsx
@@ -0,0 +1,163 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./writePage.module.css";
+import { useEffect, useState } from "react";
+import "react-quill/dist/quill.bubble.css";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import {
+    getStorage,
+    ref,
+    uploadBytesResumable,
+    getDownloadURL,
+} from "firebase/storage";
+import { app } from "@/utils/firebase";
+import ReactQuill from "react-quill";
+
+const WritePage = () => {
+    const { status } = useSession();
+    const router = useRouter();
+
+    const [open, setOpen] = useState(false);
+    const [file, setFile] = useState(null);
+    const [media, setMedia] = useState("");
+    const [value, setValue] = useState("");
+    const [title, setTitle] = useState("");
+    const [catSlug, setCatSlug] = useState("");
+
+    useEffect(() => {
+        const storage = getStorage(app);
+        const upload = () => {
+            const name = new Date().getTime() + file.name;
+            const storageRef = ref(storage, name);
+
+            const uploadTask = uploadBytesResumable(storageRef, file);
+
+            uploadTask.on(
+                "state_changed",
+                (snapshot) => {
+                    const progress =
+                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log("Upload is " + progress + "% done");
+                    switch (snapshot.state) {
+                        case "paused":
+                            console.log("Upload is paused");
+                            break;
+                        case "running":
+                            console.log("Upload is running");
+                            break;
+                    }
+                },
+                (error) => { },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        setMedia(downloadURL);
+                    });
+                }
+            );
+        };
+
+        file && upload();
+    }, [file]);
+
+    if (status === "loading") {
+        return <div className={styles.loading}>Loading...</div>;
+    }
+
+    if (status === "unauthenticated") {
+        router.push("/");
+    }
+
+    const slugify = (str) =>
+        str
+            .toLowerCase()
+            .trim()
+            .replace(/[^\w\s-]/g, "")
+            .replace(/[\s_-]+/g, "-")
+            .replace(/^-+|-+$/g, "");
+
+    const handleSubmit = async () => {
+        const res = await fetch("/api/posts", {
+            method: "POST",
+            body: JSON.stringify({
+                title,
+                desc: value,
+                img: media,
+                slug: slugify(title),
+                catSlug: catSlug || "style", //If not selected, choose the general category
+            }),
+        });
+
+        if (res.status === 200) {
+            const data = await res.json();
+            router.push(`/posts/${data.slug}`);
+        }
+    };
+
+    return (
+        <div className={styles.container}>
+            <input
+                type="text"
+                placeholder="Title"
+                className={styles.input}
+                onChange={(e) => setTitle(e.target.value)}
+            />
+            <select className={styles.select} onChange={(e) => setCatSlug(e.target.value)}>
+                <option value="style">style</option>
+                <option value="fashion">fashion</option>
+                <option value="food">food</option>
+                <option value="culture">culture</option>
+                <option value="travel">travel</option>
+                <option value="coding">coding</option>
+            </select>
+            <div className={styles.editor}>
+                <button className={styles.button} onClick={() => setOpen(!open)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </button>
+                {open && (
+                    <div className={styles.add}>
+                        <input
+                            type="file"
+                            id="image"
+                            onChange={(e) => setFile(e.target.files[0])}
+                            style={{ display: "none" }}
+                        />
+                        <button className={styles.addButton}>
+                            <label htmlFor="image">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                                </svg>
+                            </label>
+                        </button>
+                        <button className={styles.addButton}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                            </svg>
+                        </button>
+                        <button className={styles.addButton}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                <path strokeLinecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
+                            </svg>
+
+                        </button>
+                    </div>
+                )}
+                <ReactQuill
+                    className={styles.textArea}
+                    theme="bubble"
+                    value={value}
+                    onChange={setValue}
+                    placeholder="Tell your story..."
+                />
+            </div>
+            <button className={styles.publish} onClick={handleSubmit}>
+                Publish
+            </button>
+        </div>
+    );
+};
+
+export default WritePage;
